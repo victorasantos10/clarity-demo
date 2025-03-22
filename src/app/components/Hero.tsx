bb049711352c69e9c5ef01ebf20eb8a32b013917
Victor <victoralencar_stos@hotmail.com>
2025-03-22 12:45:12 -0300

    removing unused component

--- src/app/components/Hero.tsx
+++
@@ -1,5 +0,0 @@
-
-export const Hero = (props: any) => {
-    return (<></>
-        );
-}
--- src/app/lib/storyblok.ts
+++ src/app/lib/storyblok.ts
@@ -1,5 +1,4 @@
 import { apiPlugin, storyblokInit } from "@storyblok/react/rsc";
-import { Hero } from "../components/Hero";
 import { BlockBasedPage } from "../components/BlockBasedPage";
 import { MainArticlesPreview } from "../components/MainArticlesPreview";
 import BestBookmakersPage from "../melhores-casas-de-apostas/page";
@@ -10,7 +9,6 @@ export const getStoryblokApi = storyblokInit({
   use: [apiPlugin],
   components: {
     ["melhores-casas-de-apostas"]: BestBookmakersPage,
-    hero: Hero,
     block_based_page: BlockBasedPage,
     article_based_page: ArticleBasedPage,
     main_articles_preview: MainArticlesPreview
